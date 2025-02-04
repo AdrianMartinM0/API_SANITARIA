da3770e82f30908724901966c75986ff0b3933b6
Adrián Martín Muñoz <adrianmartinm04@gmail.com>
2025-02-04 13:16:34 +0100

    Finalizacion del modelo de Usuario a falta de confirmar si es necesario el centro.
    validacion del campo nombre
    
    validacion del campo apellidos
    
    validacion del campo email
    
    validacion del campo password

--- api/src/database/models/Usuario.js
+++ api/src/database/models/Usuario.js
@@ -15,34 +15,75 @@ Usuario.init(
             type: DataTypes.STRING(40),
             allowNull: false, 
             validate:{
-
-            }
+                notNull:{
+                    msg:"El campo nombre es obligatorio",
+                },
+                len:{
+                    args: [1, 40],
+                    msg: "El nombre debe tener entre 1 y 40 caracteres",
+                },
+                isAlpha:{
+                    args:true,
+                    msg:"El campo solo permite valores alfabeticos",
+                },
+            },
         },
         apellidos:{
             type: DataTypes.STRING(90),
             allowNull: false,
             validate:{
-
+                notNull:{
+                    msg:"El campo apellido es obligatorio",
+                },
+                len:{
+                    args: [1, 90],
+                    msg: "El apellido debe tener entre 1 y 90 caracteres",
+                },
+                isAlpha:{
+                    args:true,
+                    msg:"El campo solo permite valores alfabeticos",
+                },
             }
         },
         email:{
             type: DataTypes.STRING,
             allowNull: false,
             validate:{
-
+                notNull:{
+                    msg:"El campo es email obligatorio",
+                },
+                min:{
+                    args: 3,
+                    msg: "El email debe tener al menos 3 caracteres",
+                },
+                isEmail:{
+                    args: true,
+                    msg: "Debe ser un email válido",
+                },
             }
         },
         password:{
             type: DataTypes.STRING(36),
             allowNull: false,
             validate:{
-
-            }
-        }
+                notNull:{
+                    msg:"El campo password es obligatorio",
+                },
+                len:{
+                    args: [8, 36],
+                    msg: "El password debe tener entre 8 y 36 caracteres",
+                },
+                is: {
+                    args: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,36}$/,
+                    msg: "El password debe tener al menos una letra minúscula, una letra mayúscula, un número y un carácter especial",
+                },
+            },
+        },
 
         // no se si vamos a seguir manteniendo el centro
 
-    }
+    },
+    
 );
 
 module.exports = Usuario;
