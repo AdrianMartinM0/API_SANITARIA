2de8bfb47c189e7bedcf6d7e06cb7679030e9e41
ismaworkdev <ismaelworkdv@gmail.com>
2025-02-06 13:41:36 +0100

    controller Cassette and Muestra declared

---
+++ api/src/controllers/controllerCassette.js
@@ -0,0 +1,18 @@
+const { newCassete, getAllCassette, updateCassette, deleteCassete } = require('../services/serviceCassette');
+
+
+const newCassetecontroller  = async(req , res)=>{
+
+}
+
+const getAllCassettecontroller = async(req , res)=>{
+
+}
+
+const updateCassettecontroller = async(req , res)=>{
+
+}
+
+const deleteCassetecontroller = async(req , res)=>{
+
+}
---
+++ api/src/controllers/controllerMuestra.js
@@ -0,0 +1,18 @@
+const { newMuestra, getAllMuestra, updateMuestra, deleteMuestra } = require('../services/serviceMuestra');
+
+
+const putMuestracontroller = async(req , res)=>{
+
+}
+
+const getAllMuestracontroller = async(req , res)=>{
+
+}
+
+const updateMuestracontroller = async(req , res)=>{
+
+}
+
+const deleteMuestracontroller = async(req , res)=>{
+
+}
